90a6dddef626a9f1e3b9de361cc42e436470c395
CrioUser <crio-user@criodo.com>
2022-10-20 16:29:56 +0530

    Initial commit

---
+++ src/main/java/com/example/qcalc/QcalcApplication.java
@@ -0,0 +1,14 @@
+package com.example.qcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcalcApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(QcalcApplication.class, args);
+		System.out.println("Here I am!!");
+	}
+
+}
